49e3a79e73099e8c21147fafae71794526be7be5
punit355743 <punit.java.dixit@gmail.com>
2020-12-28 22:07:05 +0530

    clean all console error

--- src/components/header-component/HeaderComponent.js
+++ src/components/header-component/HeaderComponent.js
@@ -1,28 +1,28 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-function HeaderComponent(props) {
+function HeaderComponent() {
     return (
         <div>
-            <nav class="navbar navbar-expand-lg navbar-light bg-light">
-                <a class="navbar-brand" href="#">Navbar</a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
+            <nav className="navbar navbar-expand-lg navbar-light bg-light">
+                <a className="navbar-brand" href="#">Navbar</a>
+                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
                 </button>
 
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul class="navbar-nav mr-auto">
-                        <li class="nav-item active">
-                            <Link to="/"><a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a></Link>
+                <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul className="navbar-nav mr-auto">
+                        <li className="nav-item active">
+                            <Link to="/" className="nav-link">Home</Link>
                         </li>
-                        <li class="nav-item">
-                            <Link to="/about"><a class="nav-link" href="#">About</a></Link>
+                        <li className="nav-item">
+                            <Link to="/about" className="nav-link">About</Link>
                         </li>
-                        <li class="nav-item">
-                            <Link to="/help"><a class="nav-link" href="#">Help</a></Link>
+                        <li className="nav-item">
+                            <Link to="/help" className="nav-link">Help</Link>
                         </li>
-                        <li class="nav-item">
-                            <a class="nav-link disabled" href="#">Disabled</a>
+                        <li className="nav-item">
+                            <a className="nav-link disabled" href="#">Disabled</a>
                         </li>
                     </ul>
 
