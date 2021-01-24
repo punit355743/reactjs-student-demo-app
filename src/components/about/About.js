1228ba470148a29c285801121aec64487b883002
punit355743 <punit.java.dixit@gmail.com>
2021-01-24 10:08:49 +0530

    context consumer, useRef and useMemo hook implement

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { schoolContext } from "./../../store/Provider";
 
 class About extends Component {
 
@@ -17,12 +18,17 @@ class About extends Component {
 
     render() {
         return (
-            <div>
-                <h1>This is About component!!!!!</h1>
-                <h2>{this.state.name}</h2>
-                <input type="text" defaultValue="punit" />
-                <button onClick={this.clickHandler}>Click</button>
-            </div>
+            <schoolContext.Consumer>
+                {(schoolData) => (
+                    <div>
+                        <h1>{schoolData.schoolName}</h1>
+                        <h1>In Our School number of student {schoolData.student.length}</h1>
+                        <h2>{this.state.name}</h2>
+                        <input type="text" defaultValue="punit" />
+                        <button onClick={this.clickHandler}>Click</button>
+                    </div>
+                )}
+            </schoolContext.Consumer>
         );
     }
 }
